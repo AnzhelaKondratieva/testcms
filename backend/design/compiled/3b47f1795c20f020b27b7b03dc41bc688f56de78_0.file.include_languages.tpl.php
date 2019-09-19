5a570b4d320685cdda845441d2a70b8ab3970b9d
AnzhelaKondratieva <kondratieva.anzhelika92@gmail.com>
2019-09-19 19:29:11 +0300

    Initializing of project

---
+++ backend/design/compiled/3b47f1795c20f020b27b7b03dc41bc688f56de78_0.file.include_languages.tpl.php
@@ -0,0 +1,46 @@
+<?php
+/* Smarty version 3.1.33, created on 2019-09-19 16:19:49
+  from '/home/vagrant/Sites/homework5/backend/design/html/include_languages.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5d83aaa5628a59_24297048',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '3b47f1795c20f020b27b7b03dc41bc688f56de78' => 
+    array (
+      0 => '/home/vagrant/Sites/homework5/backend/design/html/include_languages.tpl',
+      1 => 1568908685,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5d83aaa5628a59_24297048 (Smarty_Internal_Template $_smarty_tpl) {
+if ($_smarty_tpl->tpl_vars['languages']->value) {?>
+    <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['languages']->value, 'lang');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['lang']->value) {
+?>
+        <a class="flag flag_<?php echo $_smarty_tpl->tpl_vars['lang']->value->id;?>
+ <?php if ($_smarty_tpl->tpl_vars['lang']->value->id == $_smarty_tpl->tpl_vars['lang_id']->value) {?> focus<?php }?> hint-bottom-middle-t-info-s-small-mobile  hint-anim" data-hint="<?php echo htmlspecialchars($_smarty_tpl->tpl_vars['lang']->value->name, ENT_QUOTES, 'UTF-8', true);?>
+" href="<?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['url'][0], array( array('lang_id'=>$_smarty_tpl->tpl_vars['lang']->value->id),$_smarty_tpl ) );?>
+" data-label="<?php echo $_smarty_tpl->tpl_vars['lang']->value->label;?>
+">
+            <?php if (is_file(((string)$_smarty_tpl->tpl_vars['config']->value->lang_images_dir).((string)$_smarty_tpl->tpl_vars['lang']->value->label).".png")) {?>
+                <img src="<?php echo call_user_func_array($_smarty_tpl->registered_plugins[ 'modifier' ][ 'resize' ][ 0 ], array( (((string)$_smarty_tpl->tpl_vars['lang']->value->label).".png"),32,32,false,$_smarty_tpl->tpl_vars['config']->value->lang_resized_dir ));?>
+" width="32px;" height="32px;">
+            <?php }?>
+        </a>
+    <?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);
+}
+}
+}
---
+++ config/config.php
@@ -0,0 +1,100 @@
+;<? exit(); ?>
+
+license = bsunqtodes hqegejhzss nvoxmlzmxn ymurvtvywr ubyrat1ved jm4rxkmgmm zqsijftkik rhzsmrrrko zwkrstp8xs tcqcvascrr 8ijlnalfre zs7evmvtnj jxxzjstwqs qxlvruyywr vexlxpsare xopg9razuu yokfjjltzz cynozyzrnu pqm7nhwlj4 z5pkopscqp porzsivohv bgnj9zlluu 8klqhpltdo jtpxsmlsrb jfwoyereua xqskzyrjyx 8i6k4qet5o kuozomqnd8 mbomrfvqha tlidtjnsps npqrpjosxw yizz7otv5p 5v5tcu93hi ekp5zmwoi7 fploydtljt jdpvnwuyvp tzzlqzxvzo xrmqye8ngr 7p84iud6 iougohxduw ntkjxhunln tlwsosyqqv xqkrlljfoc egmtmzyblp reigpcwnts wjiwoomjjp mktonqysrr qnvds77sfo 6txeywqrll om9rbdewzl qtwijwhzup zpyoxnonnr xmudxjqari 4i8a4xbgif jcgngzlebk uozsexxsht gnprzrocsw uepds8ulwr wfuu9cgthq uqlopsuztp amgtdzkqux irjuuim3jo xyprsuwxv8 pvuyzgro2z my9pjowqoy xwrpeoospw zvraxdi6zn rko6mal7uh qepeoeyhqj xm1vjxnpop mocvwu9scz o5yuwavfok n8ssjcmqyh lsywpiqypt yrvuusxylk glnwmsyrcs d6n4n9cwq7 sksgqlpyln iqzgjpmyoy nhqkqzsnwo cw8pavt389 searrchaca ysperfnusd jnjlhyuqts tvwpvwxwzs yoyr6xzn8r sffdgmzwap
+
+[database]
+
+;Сервер базы данных
+db_server = 127.0.0.1
+
+;Пользователь базы данных
+db_user = root
+
+;Пароль к базе
+db_password = "secret"
+
+;Имя базы
+db_name = testcms
+
+;Драйвер базы данных
+db_driver = mysql
+
+;Префикс для таблиц
+db_prefix = ok_
+
+;Кодировка базы данных
+db_charset = UTF8MB4
+
+;Режим SQL
+db_sql_mode = "STRICT_TRANS_TABLES,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"
+
+;Смещение часового пояса
+;db_timezone = +04:00
+
+[php]
+error_reporting = E_ALL
+php_charset = UTF8
+php_locale_collate = ru_RU
+php_locale_ctype = ru_RU
+php_locale_monetary = ru_RU
+php_locale_numeric = ru_RU
+php_locale_time = ru_RU
+;php_timezone = Europe/Moscow
+debug_mode = false
+
+[smarty]
+smarty_compile_check = true
+smarty_caching = false
+smarty_cache_lifetime = 0
+smarty_debugging = false
+smarty_html_minify = false
+smarty_security = true
+
+[design]
+debug_translation = false
+scripts_defer = true
+
+[images]
+;Указываем какую библиотеку использовать для нарезки изображений. Варианты: Gregwar, Imagick или GD. Это имя класса адаптера
+resize_adapter = Gregwar
+
+;Директория общих изображений дизайна (лого, фавикон...)
+design_images = files/images/
+
+;Файл изображения с водяным знаком
+watermark_file = backend/files/watermark/watermark.png
+
+;Промо изображения
+special_images_dir = files/special/
+
+;Директория оригиналов и нарезок фоток товаров
+original_images_dir = files/originals/products/
+resized_images_dir = files/resized/products/
+
+;Изображения оригиналов и нарезок фоток блога
+original_blog_dir = files/originals/blog/
+resized_blog_dir = files/resized/blog/
+
+;Изображения оригиналов и нарезок фоток брендов
+original_brands_dir = files/originals/brands/
+resized_brands_dir = files/resized/brands/
+
+;Изображения оригиналов и нарезок фоток категории
+original_categories_dir = files/originals/categories/
+resized_categories_dir = files/resized/categories/
+
+;Изображения оригиналов и нарезок фоток доставки
+original_deliveries_dir = files/originals/deliveries/
+resized_deliveries_dir = files/resized/deliveries/
+
+;Изображения оригиналов и нарезок фоток способов оплаты
+original_payments_dir = files/originals/payments/
+resized_payments_dir = files/resized/payments/
+
+;Изображения баннеров
+banners_images_dir = files/originals/slides/
+resized_banners_images_dir = files/resized/slides/
+
+; Папка изображений языков
+lang_images_dir = files/originals/lang/
+lang_resized_dir = files/resized/lang/
